c35ab7098840e76e31aecda85582a6fd809fbbf9
Alejandro Quintanar <alejandro@quihel.net>
2026-09-11 23:08:36 +0200

    term39: 1.5.1 -> 1.5.2

--- pkgs/by-name/te/term39/package.nix
+++ pkgs/by-name/te/term39/package.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "term39";
-  version = "1.5.1";
+  version = "1.5.2";
   __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "alejandroqh";
     repo = "term39";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-IFJXBTjGedDl+FY7HDE5p+jYqA3c5EHHxQfn8hPY/es=";
+    hash = "sha256-DjseOO3oSNdsYvl+L+x9g45cwwbEWKf33w5CIj/iEI4=";
   };
 
-  cargoHash = "sha256-RguJr5xtuLcn4OO/8hmg06AmUT6WPzGtCpB0J+xw8+I=";
+  cargoHash = "sha256-W6kYUEYsbic+Oc0CVYjrb1q35jJkuzd9ierPeMWDPXs=";
 
   nativeBuildInputs = [
     pkg-config
@@ -32,6 +32,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Modern, retro-styled terminal multiplexer with a classic MS-DOS aesthetic";
     homepage = "https://github.com/alejandroqh/term39";
+    changelog = "https://github.com/alejandroqh/term39/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ alejandroqh ];
     mainProgram = "term39";
